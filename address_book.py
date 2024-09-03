5c5eada631374ff30724ff70dc2ce62a7dda68f4
Sarvesh Shelke <sarveshshelke01@gmail.com>
2024-09-03 14:59:20 +0530

    Add: Python code to create contact in Address Book

--- address_book.py
+++ address_book.py
@@ -1,2 +1,58 @@
+def menu():
+    """
+        Description:
+        This fuction prints menu for user operations.
+        Parameter:
+        None
+        Return:
+        choice: User's choice for operation.
+    """
+    print("Welcome to Address Book\nEnter any option given below:")
+    print("1. Add the contacts in address book")
+    choice = int(input("Enter your choice: "))
+    return choice
 
-print("Welcome to Address Book Program on dev Branch")
+def create_contact():
+    """
+        Description:
+        This fuction takes input from user for each parameter
+        Parameter:
+        None
+        Return:
+        None
+    """
+    
+    first_name = input("Enter First Name: ")
+    last_name = input("Enter Last Name: ")
+    address = input("Enter Address : ")
+    city = input("Enter City: ")
+    state = input("Enter State: ")
+    zip_code = int(input("Enter Zip Code: "))
+    phone_number = int(input("Enter Phone Number: "))
+    email = input("Enter Email address: ")
+
+
+def selection(choice):
+    """
+        Description:
+        This fuction takes input as choice.
+        Parameter:
+        choice: Choice of option from user
+        Return:
+        None
+    """
+    
+    match choice:
+        case 1:
+            create_contact()
+        case _:
+            print("Invalid choice. Please try again.")
+
+def main():
+    
+    user_choice = menu()
+    selection(user_choice)
+    
+if __name__ == "__main__":
+    main()
+        
